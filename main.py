75e65ca30a2d8bf29b9e2ff00d5608b668b3e858
Faizan Sayed <baa98989@gmail.com>
2024-06-13 03:43:13 -0400

    minor debudding

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ window.resizable(False, False)
 score = 0
 direction = 'down'
 
-label = Label(window, text='score:{}'.format(score), font=('consolas, 40'))
+label = Label(window, text='score:{}'.format(score), font=('consolas', 40))
 label.pack()
 
 canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
